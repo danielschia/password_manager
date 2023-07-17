f5c53248eb999d7acfb0aa598d665694625d86bb
danielschia <daniel.schia@gmail.com>
2023-07-17 14:09:54 +0100

    set nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :passwords
+  resources :passwords do
+    resources :shares, only: [:new, :create, :destroy] #user_passwords
+  end
 
   root "passwords#index"
 end
