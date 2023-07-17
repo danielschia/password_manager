ed669513ec2ff1fc9d88e3cc19c5a61caccae8b0
danielschia <daniel.schia@gmail.com>
2023-07-17 14:57:14 +0100

    refactor controllers

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -14,7 +14,8 @@ class PasswordsController < ApplicationController
   end
 
   def create
-    @password = current_user.passwords.create(password_params)
+    @password = Password.new(password_params)
+    @password.user_passwords(user: current_user, role: :owner)
     if @password.persisted?
       redirect_to @password
     else
